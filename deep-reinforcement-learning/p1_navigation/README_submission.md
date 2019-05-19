6d8c03d7a33a571124232de2e29287bb6f6280cc
houstonj2013 <jerome198246@gmail.com>
2019-05-19 09:46:09 -0500

    per

--- deep-reinforcement-learning/p1_navigation/README_submission.md
+++ deep-reinforcement-learning/p1_navigation/README_submission.md
@@ -61,7 +61,15 @@ python -m unittest discover unit_tests -v
 
 ## To experiment with different parameters and environment (Defaut is LunarLander-v2)
 ```
-python agent_experiment.py --device gpu 
+python agent_experiment.py --device gpu ## Ordinary replaybuffer
+python agent_experiment.py --device gpu --per per ## Prioritized Replay Buffer
 
 python agent_experiment.py --device gpu --env Banana_unity --max_t 1000 --batch_size 64 --num_episodes 200 --score_threshold 13 --score_window_size 5 --update_every 10 --eps_decay 0.99
 ```
+
+## Setup Note
+In order to run LunarLander-v2
+
+gym 0.10.8
+conda install swig 
+pip install box2d-py
--- deep-reinforcement-learning/p1_navigation/unit_tests/test_dqn_agent.py
+++ deep-reinforcement-learning/p1_navigation/unit_tests/test_dqn_agent.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import numpy as np
-from dqn.dqn_agent import Agent, ReplayBuffer
+from dqn.dqn_agent import Agent, ReplayBuffer, PrioritizedReplayBuffer
 import torch
 
 
@@ -78,6 +78,71 @@ class AgentTest(TestCase):
         left, right = target[terminate_index], rewards[terminate_index]
         self.assertTrue(torch.all(torch.eq(left, right)))
 
+    def test_abs_error(self):
+        qnet = torch.rand(self.batch_size).reshape(-1, 1)
+        target = torch.rand(self.batch_size).reshape(-1, 1)
+        abs_error = self.agent._abs_error(qnet, target)
+        print(abs_error.shape)
+        self.assertTrue(abs_error.shape == torch.Size([self.batch_size]))
+
+
+    def test_weighted_mse_loss(self):
+        qnet = torch.from_numpy(np.array([2.0, 5.0, 1.0, 1.0])).reshape(-1, 1)
+        target = torch.from_numpy(np.array([0.0, 0.0, 0.0, 0.0])).reshape(-1, 1)
+        weights = torch.from_numpy(np.array([0.5, 0.2, 1, 1])).reshape(-1, 1)
+        loss = self.agent._weighted_mse_loss(qnet, target, weights)
+        self.assertTrue(loss == 2.25)
+        pass
+
+    def test_per_option(self):
+        seed = 30
+        self.agent = Agent(state_size=self.state_size, 
+                           action_size=self.action_size, 
+                           batch_size=self.batch_size,
+                           seed=seed, 
+                           per=True,
+                           device=torch.device("cpu"))
+        self.test_act()
+        self.test_evaluate_target()
+        self.test_abs_error()
+        self.test_weighted_mse_loss()
 
 
     
+class PrioritizedReplayBufferTest(TestCase):
+
+    def setUp(self):
+        self.action_size = 1
+        self.buffer_size = 1000
+        self.batch_size = 64
+        seed=42
+        self.replaybuffer = PrioritizedReplayBuffer( 
+                                 action_size=self.action_size, 
+                                 buffer_size=self.buffer_size,
+                                 batch_size=self.batch_size,
+                                 seed=seed, 
+                                 device=torch.device("cpu"))
+
+    def test_add(self):
+        state, action, reward, next_state, done = \
+            np.array([3, 4, 1]), np.array([2]), 3, np.array([2, 3, 5]), False
+        for i_ in range(2 * self.buffer_size):
+            self.replaybuffer.add(state, action, reward, next_state, done)
+            if i_ < self.buffer_size:
+                ## Initial weight for each add is 1 
+                self.assertEqual(self.replaybuffer.memory.total_p, i_ + 1)
+    
+    def test_sample(self):
+        
+        for i_ in range(2 * self.buffer_size):
+            state, action, reward, next_state, done = \
+            np.array([3, 4, i_]), np.array([2]), 3, np.array([2, 3, 5]), False
+            self.replaybuffer.add(state, action, reward, next_state, done)
+        tree_idx, batch_memory, ISWeights = self.replaybuffer.sample()
+        states, actions, rewards, next_states, dones = batch_memory
+        print(ISWeights)
+        # Test size
+        self.assertEqual(actions.shape, torch.Size([self.batch_size, self.action_size]))
+        self.assertEqual(rewards.shape, torch.Size([self.batch_size, 1]))
+        self.assertEqual(dones.shape, torch.Size([self.batch_size, 1]))
+        self.assertEqual(ISWeights.shape, torch.Size([self.batch_size, 1]))
