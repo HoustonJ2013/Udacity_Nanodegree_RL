040a48542eb211d79708eee285adf9c0679b7ab1
jingbo <jerome198246@gmail.com>
2019-05-26 22:05:48 -0500

    p2

---
+++ deep-reinforcement-learning/p2_continuous-control/agent_experiment.py
@@ -0,0 +1,145 @@
+import numpy as np
+import random
+import argparse
+from collections import namedtuple, deque
+from unityagents import UnityEnvironment
+import gym
+from agents.dqn_agent import UnityEnvironment, Agent, ReplayBuffer
+from agents.Unity_Env import UnityEnv_Reacher
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import pickle
+from time import gmtime, strftime
+
+def main(args):
+
+    ## Support two environment, LunarLander was used to debug 
+    if args.env == "Reacher_unity":
+        env = UnityEnv_Reacher(env_file="Reacher_Linux/Reacher.x86_64")
+        state_size = env.state_size
+        action_size = env.action_size
+    elif args.env == "Reacher_unity_v2":
+        env = UnityEnv_Reacher(env_file="Reacher_Linux_20/Reacher.x86_64")
+        state_size = env.state_size
+        action_size = env.action_size
+    
+    ## Enviornment description 
+    print("Current environment is ", args.env)
+    print("State size is %i"%(state_size))
+    print("action size is %i"%(action_size))
+    print("A typical state looks like", env.reset())
+
+    ## device for training
+    if args.device.lower() == "cpu".lower():
+        device = torch.device("cpu")
+    else:
+        device = torch.device("cuda:0")
+
+    ## Whether to use prioritized replay buffer
+    use_prioritized_replay = args.per
+    if args.per:
+        print("Using Prioritized Replay")
+
+    agent = Agent(state_size=state_size, 
+                  action_size=action_size, 
+                  seed=42,
+                  buffer_size=args.buffer_size,
+                  batch_size=args.batch_size,
+                  gamma=args.gamma,
+                  tau=args.tau, 
+                  lr=args.lr,
+                  update_every=args.update_every,
+                  per=use_prioritized_replay, 
+                  device=device, 
+                  loss=args.loss,
+                 )
+    # watch an untrained agent
+    # state = env.reset()
+    # for j in range(200):
+    #     action = agent.act(state)
+    #     env.render()
+    #     state, reward, done, _ = env.step(action)
+    #     if done:
+    #         break 
+    # env.close()
+
+    ## Train the agent
+    n_episodes = args.num_episodes
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=args.score_window_size)  # last 100 scores
+    game_length = deque(maxlen=args.score_window_size)
+    eps = args.eps_start                    # initialize epsilon
+
+    ## Current time string 
+    now_string = strftime("%Y_%m_%d_%H_%M_%S", gmtime()) 
+
+    for i_episode in range(1, n_episodes+1):
+        state = env.reset()
+        score = 0
+        game_len = 0
+        for t in range(args.max_t):
+            action = agent.act(state, eps)
+            next_state, reward, done, _ = env.step(action)
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            game_len += 1
+            if done:
+                break 
+        game_length.append(game_len)
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(args.eps_end, args.eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f} current eps :{:.4f} Q Network Running Loss: {:.05f}'.format(i_episode, 
+                                                np.mean(scores_window), 
+                                                eps, 
+                                                np.mean(agent.running_loss)), end="")
+        if i_episode % args.score_window_size == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f} and Average game length {} current eps {:.04f} running_loss {}'\
+                .format(i_episode, np.mean(scores_window), np.mean(game_length), eps, np.mean(agent.running_loss)))
+        if np.mean(scores_window)>=args.score_threshold or i_episode == n_episodes:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f} current eps :{:.4f}'.format(i_episode-100, np.mean(scores_window), eps))
+            check_point_name = args.env + "_episodes_" + str(i_episode) + "_score_" + str(np.mean(scores_window)) + now_string + \
+                "_" +  args.testname + "_checkpoint.pth"
+            torch.save(agent.qnetwork_local.state_dict(), "models/" + check_point_name)
+            break
+    
+    score_name = args.env + "_episodes_" + str(i_episode) + "_score_" + str(np.mean(scores_window)) + now_string + "_" +  args.testname +  "_scores"
+    np.save("models/" + score_name, scores)
+    # replay_buffer_name = args.env + now_string + "_replay_buffer_" + args.testname
+    # replay_buffer = [tem_._asdict() for tem_ in list(agent.memory.memory)]
+    # with open("models/" + replay_buffer_name, "wb") as pickle_file:
+    #     pickle.dump(replay_buffer, pickle_file)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    ## Agent related parameters
+    parser.add_argument('--per', action="store_true", default=False)
+    parser.add_argument('--buffer_size', type=int, default=int(1e4))
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--tau', type=float, default=1e-3)
+    parser.add_argument('--lr', type=float, default=5e-4)
+    parser.add_argument('--update_every', type=int, default=4)
+    parser.add_argument('--score_threshold', type=int, default=200)
+    parser.add_argument('--score_window_size', type=int, default=100)
+
+    ## Environment related parameters
+    parser.add_argument('--env', type=str, default="Reacher_unity")
+    parser.add_argument('--num_episodes', type=int, default=2000,
+                        help='no. of epoches to train the model')
+    parser.add_argument('--eps_start', type=float, default=1.0)
+    parser.add_argument('--eps_end', type=float, default=0.01)
+    parser.add_argument('--eps_decay', type=float, default=0.999)
+    parser.add_argument('--max_t', type=int, default=1000)
+    parser.add_argument('--loss', type=str, default="mse")
+
+    parser.add_argument('--workers', type=int, default=8)
+    parser.add_argument('--device', type=str, default="cpu")
+
+    parser.add_argument('--testname', type=str, default="")
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
---
+++ deep-reinforcement-learning/p2_continuous-control/agents/Unity_Env.py
@@ -0,0 +1,55 @@
+from unityagents import UnityEnvironment
+import numpy as np
+
+
+REACHER_STATES_MIN = np.array([ -4,  -4,  -4,  -1,  -1,  -1,  -1, -10,  -2,  -3, -14, -10, -13,
+        -9, -10,  -9,  -1,  -1,  -1,  -1, -11, -10,  -6, -19, -20, -18,
+        -8,  -1,  -8,  -1,  -1,  -1,  -1])
+REACHER_STATES_MAX = np.array([4,  4,  4,  1,  1,  1,  1, 10,  2,  3, 14, 10, 13,  9, 10,  9,  1,
+        1,  1,  1, 11, 10,  6, 19, 20, 18,  8,  1,  8,  1,  1,  1,  1])
+
+class UnityEnv_Reacher():
+    '''
+    Simply the syntax for Unity environment, make it similar to gym
+    Dedicated for banana environment. The state scaling is hard coded 
+    for banana
+    '''
+    def __init__(self, env_file, states_min=REACHER_STATES_MIN, states_max=REACHER_STATES_MAX):
+        
+        ## env_file is the unity environment file
+        ## states_min min for the state values  (n_states, )
+        ## states_max max for the state values  (n_states, )
+        self.env = UnityEnvironment(file_name=env_file)
+        self.brain_name = self.env.brain_names[0]
+        self.env_info = self.env.reset(train_mode=True)[self.brain_name]
+        self.num_agents = len(self.env_info.agents)
+        self.brain = self.env.brains[self.brain_name]
+        self.action_size = self.brain.vector_action_space_size
+        state = self.env_info.vector_observations[0]
+        self.state_size = len(state)
+        self.state_min = states_min
+        self.state_max = states_max
+
+    def reset(self, train_mode=True):
+        self.env_info = self.env.reset(train_mode=train_mode)[self.brain_name]
+        ## return shape self.num_agents x self.state_size
+        return self._state_scaler(self.env_info.vector_observations)
+
+    def step(self, action):
+        self.env_info = self.env.step(action)[self.brain_name]
+        next_state = self.env_info.vector_observations
+        reward = self.env_info.rewards
+        done = self.env_info.local_done 
+        ## return shape self.num_agents x item size
+        return self._state_scaler(next_state), np.array(reward), np.array(done), "dummy"
+
+    def render(self):
+        return 
+
+    def close(self):
+        self.env.close()
+
+    def _state_scaler(self, state):
+        ## state self.num_agents x self.state_size
+        state_scaled = (state - self.state_min) / (self.state_max - self.state_min)
+        return state_scaled
